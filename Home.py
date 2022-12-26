a6c9add15752601ae8e98b47729c0957c3215eaf
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2022-12-26 23:21:16 +0100

    First scatter plot without interactive variables

---
+++ Home.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+from math import sqrt
+from random import choice
+
+def create_new_point(old_point, base_points):
+    ref = choice(base_points)
+    new_point = np.sum([old_point, ref], axis = 0)
+    new_point = np.dot(0.5, new_point)
+    return new_point
+
+
+st.title("Sierpinski Triangle")
+
+base_points = [(0,0), (1,0), (0.5, sqrt(3)/2)]
+points = []
+start = (0.5,0.5)
+points.append(start)
+old_point = start
+for i in range(10000):
+    new_point = tuple(create_new_point(old_point, base_points))
+    points.append(new_point)
+    old_point = new_point
+
+
+
+scale = 1000
+scaled_basepoints = [(scale*x, scale*y) for x,y in base_points]
+
+fig = plt.figure()
+x, y = zip(*base_points)
+plt.scatter(x,y)
+x,y = zip(*points)
+plt.scatter(x,y)
+
+st.pyplot(fig)
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# Sierpi´nski Triangle
+
+Lets use python/streamlit to visualise the creation of a Sierpi´nski triangle using the chaos game.
+
+## The chaos game.
+
+1. Take three points in a plane to form a triangle
+2. Randomly select any point inside the triangle.
+3. Randomly select any of the three vertex points.
+4. Move half of the distance from your current position to the selecter vertex.
+5. Plot the current position.
+6. Repeat from step 3.
+
+Source : [Wiki:Sierpinski](https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle)  
